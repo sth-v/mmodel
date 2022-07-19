b1bbdf4cb5de5da1f1e8e19a7b39d82b07c2f465
Sofya Dobychina <96266344+sf-d@users.noreply.github.com>
2022-07-19 23:02:44 +0300

    draft

--- lahta/items.py
+++ lahta/items.py
@@ -1,5 +1,5 @@
 from mm.baseitems import Item
-
+from compas.geometry import Point, Polygon
 
 class Element(Item):
     def __init__(self, *args, **kwargs):
@@ -11,6 +11,29 @@ class Element(Item):
 
 class Panel(Item):
     # __init__ нужен только когда требуется задать специфический порядок первой инициализации
+
     def __call__(self, *args, **kwargs):
         super().__call__(*args, **kwargs)
-        # Здесь писать все что нужно для инициализации
+        self.vertices = kwargs['vertices']
+        self.perforation = kwargs['perforation']
+
+    def panel_shape(self):
+        return Polygon(self.vertices)
+
+    @property
+    def perforation(self):
+        return self._perforation
+
+    @property
+    def hole(self):
+        self.hole = 5
+        return self.hole
+
+    # для того чтобы пометить дырку, нужно получить объект пересекающийся с панелью (возможно от другого класса)
+    @hole.setter
+    def hole(self, value):
+        self._hole = value
+
+    @perforation.setter
+    def perforation(self, value):
+        self._perforation = value
