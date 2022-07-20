9f37e3744fe820ae595dde24d177fb5e998620ba
Sofya Dobychina <96266344+sf-d@users.noreply.github.com>
2022-07-21 01:26:17 +0300

    draft_1

--- lahta/items.py
+++ lahta/items.py
@@ -1,5 +1,6 @@
 from mm.baseitems import Item
-from compas.geometry import Point, Polygon
+from compas.geometry import Point, Polygon, offset_polyline, Polyline, offset_polygon
+
 
 class Element(Item):
     def __init__(self, *args, **kwargs):
@@ -8,30 +9,96 @@ class Element(Item):
     def __call__(self, *args, **kwargs):
         super().__call__(*args, **kwargs)
 
-class Panel(Item):
-    # __init__ нужен только когда требуется задать специфический порядок первой инициализации
+
+# геометрические примитивы
+class PolygonObj(Item):
     def __call__(self, *args, **kwargs):
         super().__call__(*args, **kwargs)
-        self.vertices = kwargs['vertices']
-        self.perforation = kwargs['perforation']
 
-    def panel_shape(self):
+        try:
+            self.vertices = args[0].tolist()
+            if self.vertices[0] == self.vertices[-1]:
+                del self.vertices[-1]
+        except AttributeError:
+            self.vertices = args[0]
+            if self.vertices[0] == self.vertices[-1]:
+                del self.vertices[-1]
+
+        self.polygon = self.get_poly()
+        self.polygon_lines = self.get_lines()
+
+    def get_poly(self):
         return Polygon(self.vertices)
 
-    @property
-    def perforation(self):
-        return self._perforation
+    def get_lines(self):
+        return self.polygon.lines
 
-    @property
-    def hole(self):
-        self.hole = 5
-        return self.hole
+    def poly_offset(self, dist):
+        return offset_polygon(self.vertices, dist)
 
-    # для того чтобы пометить дырку, нужно получить объект пересекающийся с панелью (возможно от другого класса)
-    @hole.setter
-    def hole(self, value):
-        self._hole = value
 
-    @perforation.setter
-    def perforation(self, value):
-        self._perforation = value
+
+
+
+
+# разные типы отгибов
+class BendProfile_A(Item):
+    def __call__(self, *args, **kwargs):
+        super().__call__(*args, **kwargs)
+        self.profile = self.profile_geometry()
+
+    def profile_geometry(self):
+        return self
+
+
+class BendProfile_B(Item):
+    def __call__(self, *args, **kwargs):
+        super().__call__(*args, **kwargs)
+        self.profile = self.profile_geometry()
+
+    def profile_geometry(self):
+        return self
+
+
+
+# Отгиб панели - профиль получается в зависимости от типа
+class PanelBend(Item):
+    def __call__(self, *args, **kwargs):
+        super().__call__(*args, **kwargs)
+
+        # пишу пока просто для общего понимания, что за тип данных будет передан в класс
+        self.grid_hash = kwargs['grid_hash']  # identical for panel and bend
+        self.side = kwargs['side']  # integer
+        self.type = kwargs['type']  # bend type name str
+
+        # получение профиля отгиба в зависимости от типа
+        if self.type == 'A':
+            self.profile = BendProfile_A().profile
+        elif self.type == 'B':
+            self.profile = BendProfile_B().profile
+
+
+# Сама панель
+class Panel(Item):
+    def __call__(self, *args, **kwargs):
+        super().__call__(*args, **kwargs)
+
+        # пишу пока просто для общего понимания, что за тип данных будет передан в класс
+        self.grid_hash = kwargs['grid_hash']  # identical for panel and bend
+        self.vertices = kwargs['vertices']  # фактическое положение вершин
+
+        #self.perforation = kwargs['perforation']
+
+    # линия офсета от панели в осях, внешний край загиба (до радиуса)
+    def panel_outer_bend_line(self):
+        dist = 7.5  # may be changed
+        frame = PolygonObj(self.vertices)
+        print(frame)
+        return frame.poly_offset(dist)
+
+
+b = Panel(grid_hash='1234', vertices = [[-1383.220328, 1499.49728, -160.132],
+[-882.411001, 2121.091646, 186.82],
+[-448.874568, 1451.682329, -186.82],[-1383.220328, 1499.49728, -160.132]
+])
+print(b.panel_outer_bend_line())
