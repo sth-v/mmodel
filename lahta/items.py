d3331d1e20c38e2ead060dffc7352048fa7e43d7
Sofya Dobychina <96266344+sf-d@users.noreply.github.com>
2022-07-28 03:05:02 +0300

    draft_2608

--- lahta/items.py
+++ lahta/items.py
@@ -1,11 +1,13 @@
 from __future__ import print_function
 import math
 # import rhino3dm
+from tools.geoms import OCCNurbsCurvePanels
 import numpy as np
 from mm.baseitems import Item
 from compas.geometry import Point, Polygon, offset_polyline, Polyline, offset_polygon, normal_polygon, Plane, \
-    translate_points, Circle, Frame, Transformation
-from compas_occ.geometry import OCCNurbsCurve
+    translate_points, Circle, Frame, Transformation, NurbsCurve, Vector
+from compas_occ.geometry import OCCNurbsCurve, OCCCurve
+from compas_view2.app import App
 
 np.set_printoptions(suppress=True)
 
@@ -18,6 +20,12 @@ class Element(Item):
         super().__call__(*args, **kwargs)
 
 
+#
+#
+#
+#
+#
+#
 # геометрические примитивы
 class PolygonObj(Item):
     def __call__(self, *args, **kwargs):
@@ -59,103 +67,86 @@ class PointObj(Item):
 
     def translate_points(self, vector):
         tr = translate_points([self.point], vector)[0]
-        return PointObj(tr)
-
-
+        return PointObj(tr).point
 #
 #
 #
 #
 #
 #
-#
-
-class FoldElement():
-    def __init__(self, radius, angle, base_axe, fold_dir, normal, *args, **kwargs):
+# Элемент отгиба
+class FoldElement:
+    def __init__(self, radius, angle, *args, **kwargs):
         super(FoldElement, self).__init__(*args, **kwargs)
-
         self.radius = radius  # radius of the fold
-        self.angle = angle  # part of the circle required for fold
-        self.base_axe = base_axe  # compas line
-        self.fold_dir = fold_dir  # fold_dir - whether radius to inside ot to outside (- or + sign)
-        self.normal = normal  # poly normal
-        self.point = PointObj(self.base_axe[0])
-
-    # Возможно нужно засунуть в класс векторов
-    def get_third_vector(self):
-        side_vec = self.base_axe.vector / np.linalg.norm(self.base_axe.vector)
-        normal_vec = self.fold_dir * np.asarray(self.normal)
-        return (np.cross(side_vec, normal_vec)) / np.linalg.norm(np.cross(side_vec, normal_vec))
+        self.angle = angle  # angle between vectors of sides of bend after fillet
+        self.plane = Plane.worldXY()
 
     def circle_center(self):
-        vec = np.sum([np.asarray(self.get_third_vector()), self.fold_dir * np.asarray(self.normal)], axis=0)
-        vec = vec / np.linalg.norm(vec)
-        transl_dist = math.sqrt(2 * (self.radius ** 2))
-        transl_vec = vec * transl_dist
-        return self.point.translate_points(transl_vec).point
+        circ = Circle(self.plane, self.radius)
+        return circ
 
-    def fold_position_circle(self):
-        #pl = Plane.from_point_and_two_vectors(self.circle_center(), self.get_third_vector(), self.fold_dir * np.asarray(self.normal))
-        pl = Plane(self.circle_center(), self.base_axe.vector / np.linalg.norm(self.base_axe.vector))
-        pl_frame = Frame.from_plane(pl)
-        rotation_frame = Frame(self.circle_center(), self.get_third_vector(), self.fold_dir * np.asarray(self.normal))
-        tr = Transformation.from_frame_to_frame(pl_frame, rotation_frame)
-        circ = Circle(pl, self.radius)
-        return OCCNurbsCurve.from_circle(circ).transformed(tr)
+    def circle_param(self):
+        circ_angle = 180 - self.angle
+        return circ_angle / 360
 
-    def split_circ_at_param(self):
-        return OCCNurbsCurve.segmented(self.fold_position_circle(), 0.25, 0.5)
+    def curved_segment(self):
+        circ = OCCNurbsCurvePanels.from_circle_world(self.circle_center())
+        return OCCNurbsCurvePanels.segmented(circ, 0.0, self.circle_param())
+
+    def straight_segment_len(self):
+        full_len = self.circle_center().circumference
+        unfold = full_len * self.circle_param()
+        return unfold
 
 
+class StraightElement:
+    def __init__(self, radius, angle, *args, **kwargs):
+        super(StraightElement, self).__init__(*args, **kwargs)
+        self.radius = radius  # radius of the fold
+        self.angle = angle  # angle between vectors of sides of bend after fillet
+        self.plane = Plane.worldXY()
 
-
-
-
-
-
-
-
-
-
-
-
-
+#
+#
+#
+#
+#
+#
 # разные типы отгибов
-class BendProfile_A(Item):
-    def __call__(self, *args, **kwargs):
-        super().__call__(*args, **kwargs)
+# думаю это будет класс, который генерит профиль на основе паттерна? значений загиб - прямой кусок и тд
+
+class BendProfile:
+    def __init__(self, *args, **kwargs):
+        super(BendProfile, self).__init__(*args, **kwargs)
         self.profile = self.profile_geometry()
 
     def profile_geometry(self):
         return self
 
-
-class BendProfile_B(Item):
-    def __call__(self, *args, **kwargs):
-        super().__call__(*args, **kwargs)
-        self.profile = self.profile_geometry()
+#
+#
+#
+#
+#
+#
+# разные типы панелей
+# думаю это будет класс, который генерит профиль на основе паттерна? значений загиб - прямой кусок и тд
+class FaceProfile(StraightElement):
+    def __init__(self, angle, radius, *args, **kwargs):
+        super(FaceProfile, self).__init__(*args, **kwargs)
+        self.radius = radius
+        self.angle = angle
 
     def profile_geometry(self):
         return self
 
-
-# Отгиб панели - профиль получается в зависимости от типа
-class PanelBend(Item):
-    def __call__(self, *args, **kwargs):
-        super().__call__(*args, **kwargs)
-
-        # пишу пока просто для общего понимания, что за тип данных будет передан в класс
-        self.grid_hash = kwargs['grid_hash']  # identical for panel and bend
-        self.side = kwargs['side']  # integer
-        self.type = kwargs['type']  # bend type name str
-
-        # получение профиля отгиба в зависимости от типа
-        if self.type == 'A':
-            self.profile = BendProfile_A().profile
-        elif self.type == 'B':
-            self.profile = BendProfile_B().profile
-
-
+#
+#
+#
+#
+#
+#
 # Сама панель
 class Panel(Item):
     def __call__(self, *args, **kwargs):
@@ -164,20 +155,36 @@ class Panel(Item):
         # пишу пока просто для общего понимания, что за тип данных будет передан в класс
         self.grid_hash = kwargs['grid_hash']  # identical for panel and bend
         self.vertices = kwargs['vertices']  # фактическое положение вершин
-        self.frame = PolygonObj(self.vertices)
+        self.offset_dist = kwargs['offset_dist']
+        self.frame = self.panel_safe_offset()  # офсет до точки с расстоянием между панелями
+        self.bend_type =
 
-        # self.perforation = kwargs['perforation']
 
     # линия офсета от панели в осях, внешний край загиба (до радиуса)
-    def panel_outer_bend_line(self):
-        dist = 7.5  # may be changed
+    def panel_safe_offset(self):
         frame = PolygonObj(self.vertices)
-        return frame.poly_offset(dist)
+        offset = frame.poly_offset(self.offset_dist)
+        return PolygonObj(offset)
+
+
+
+
+
+
+
+
 
 
 b = Panel(grid_hash='1234', vertices=[[-1383.220328, 1499.49728, -160.132],
                                       [-882.411001, 2121.091646, 186.82], [-448.874568, 1451.682329, -186.82],
-                                      [-1383.220328, 1499.49728, -160.132]])
+                                      [-1383.220328, 1499.49728, -160.132]], offset_dist=10)
 
-c = FoldElement(10, 45, b.frame.polygon_lines[0], -1, b.frame.poly_normal())
-print(c.split_circ_at_param())
+print(b.frame.polygon.to_jsonstring())
+c = FoldElement(10, 135)
+poly = c.circle_center()
+#segment = c.straight_segment()
+
+# view = App(width=1600, height=900)
+# view.add(Polyline(poly.locus()), linewidth=1, linecolor=(0, 0, 0))
+# view.add(Polyline(segment.locus()), linewidth=4, linecolor=(0, 1, 0))
+# view.show()
