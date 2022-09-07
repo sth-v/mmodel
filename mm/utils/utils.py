71bae8bced4d2392c2780925f4e148c8efda5ebc
andrewastakhov <aa@contextmachine.ru>
2022-09-07 04:24:28 +0300

    DotView

--- mm/utils/utils.py
+++ mm/utils/utils.py
@@ -1,6 +1,7 @@
 #  Copyright (c) 2022. Computational Geometry, Digital Engineering and Optimizing your construction processe"
 
 from mm.baseitems import Item
+import weakref
 
 
 class ReplaceMapping:
@@ -9,6 +10,10 @@ class ReplaceMapping:
         " ": "__",
         ".": "___"
     }
+
+    def __init__(self):
+        super(ReplaceMapping, self).__init__()
+        self.replacements = self.__class__.replacements
 
     @classmethod
     def replace(cls, string: str):
@@ -25,23 +30,50 @@ class ReplaceMapping:
         return string
 
 
-class DotView(Item, ReplaceMapping):
+from typing import Mapping, MappingView
 
-    def __init__(self, dct={}, **kwargs):
+
+
+class DotView(Item, ReplaceMapping):
+    """
+    >>> class R2(ReplaceMapping):
+    ...     replacements = {
+    ...             "a": "AAAA",
+    ...             "b": "NNN",
+    ...             "c": "CCC"
+    ...         }
+
+
+    >>> class BV(DotView, R2):
+    ...     ...
+
+
+    >>> d = BV(**dict(a=6, b=dict(t=0, d=6)))
+
+    >>> d.AAAA
+    6
+    """
+    def __init__(self, dct=None, **kwargs):
         super().__init__(**kwargs)
+        if dct is None:
+            dct = dict()
         kwargs |= dct
-        self.dct = kwargs
-        DotView.do_traverse(self, kwargs)
+
+        self.do_traverse(self, kwargs)
+
+
 
     @classmethod
     def do_traverse(cls, obj, dct):
         for k, v in dct.items():
 
             if isinstance(v, dict):
-                new_item = Item()
+                new_item = cls()
 
-                obj(**{cls.replace(k): new_item})
+                obj.__call__(**{cls.replace(k): new_item})
 
                 cls.do_traverse(new_item, v)
             else:
-                obj(**{cls.replace(k): v})
+                obj.__call__(**{cls.replace(k): v})
+
+
