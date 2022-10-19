4a324e29d1a606afc9dfe8b13ab3d5e0ec3915e2
andrewastakhov <aa@contextmachine.ru>
2022-10-20 02:37:41 +0300

    buffer.py Add Tree.js geometry Support

---
+++ mm/tests/test_buffer.py
@@ -0,0 +1,19 @@
+#  Copyright (c) 2022. Computational Geometry, Digital Engineering and Optimizing your construction processe"
+import unittest
+from unittest import TestCase
+
+from mm.geom.buffer import TrimmingCone
+
+
+class TestTrimmingCone(TestCase):
+
+    def run(self, result: unittest.result.TestResult | None = ...) -> unittest.result.TestResult | None:
+        self.tc = TrimmingCone([1, 2, 3], [12, 15, 8], 5, 7)
+        return super(TestTrimmingCone, self).run(result)
+
+    def test_to_json(self):
+        self.assertIsNotNone(self.tc.to_json())
+        print(self.tc.to_json(indent=2))
+
+    def test_to_dict(self):
+        self.assertIsNotNone(self.tc.to_dict())
