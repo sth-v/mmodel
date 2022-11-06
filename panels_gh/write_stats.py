93b78a9be1389af1cee471ab45b7aef4412c9cff
sth-v <aa@contextmachine.online>
2022-11-06 17:32:27 +0300

    stats
    Signed-off-by: sth-v <aa@contextmachine.online>

--- panels_gh/write_stats.py
+++ panels_gh/write_stats.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import json
-import numpy as np
 
 # with open('panels_gh/for_stats/niche_sizes.json', 'r') as out_file:
 # data = json.load(out_file)
@@ -12,14 +10,12 @@ import numpy as np
 data = pd.read_csv('panels_gh/for_stats/221031_panels_sizes.csv')
 new = {'1': [], '2': [], '3': [], '4': [], '5': [], '6': []}
 
-for i,v in enumerate(data['names']):
+for i, v in enumerate(data['names']):
     n = v[4]
     val = data.iloc[i]
     new[str(n)].append(list(val.values))
 
-
-d = list(new.values())
-
-iterables = [*[i for i in d], *[np.repeat('1', 122), np.repeat('2', 122), np.repeat('3', 122), np.repeat('4', 122), np.repeat('5', 122), np.repeat('6', 122)]]
-#tuples = list(zip(*arrays))
-#a = pd.MultiIndex.from_product(iterables, names=["names", "height", 'width'])
+right = pd.DataFrame.from_dict(right)
+left = pd.DataFrame.from_dict(left)
+data = pd.concat([right, left], ignore_index=True)
+data.to_csv('panels_gh/221012_panels_sizes.csv', encoding='utf-8', index=True)
